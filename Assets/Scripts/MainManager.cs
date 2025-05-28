ee67d2fe063dac89aa03633b73ba43b598e0403b
Timothy Partee <timothyp2099@gmail.com>
2025-05-28 14:05:29 -0700

    Refactored Instance management
    Refactored code so that instance management is only on one file to control data persistence int the scenes.

--- Assets/Scripts/MainManager.cs
+++ Assets/Scripts/MainManager.cs
@@ -10,21 +10,38 @@ public class MainManager : MonoBehaviour
     public Brick BrickPrefab;
     public int LineCount = 6;
     public Rigidbody Ball;
+    public static MainManager Instance;
 
     public Text ScoreText;
     public Text ScoreText2;
     public GameObject GameOverText;
+    public int highScore;
     
     private bool m_Started = false;
     private int m_Points;
     private string playerName;
     
     private bool m_GameOver = false;
+    private static int loadCount = 0;
 
-    
+
+    private void Awake()
+    {
+        loadCount++;
+        if (StartMenuManager.Instance != null)
+        {
+            ScoreText2.text = "Best Score: " + StartMenuManager.Instance.LoadName() + " : " + StartMenuManager.Instance.LoadScore().ToString();
+        }
+        
+    }
     // Start is called before the first frame update
     void Start()
     {
+        if (loadCount > 1)
+        {
+            ScoreText2.text = "Best Score: " + StartMenuManager.Instance.LoadName() + " : " + StartMenuManager.Instance.LoadScore().ToString();
+        }
+       
         playerName = StartMenuManager.Instance.LoadName();
         const float step = 0.6f;
         int perLine = Mathf.FloorToInt(4.0f / step);
@@ -62,6 +79,7 @@ public class MainManager : MonoBehaviour
             if (Input.GetKeyDown(KeyCode.Space))
             {
                 SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+                
             }
         }
     }
@@ -74,23 +92,29 @@ public class MainManager : MonoBehaviour
 
     public void GameOver()
     {
-        if (m_Points > LoadNameScore())
+        if (m_Points > highScore)
         {
-            SaveNameScore(m_Points);
-            ScoreText2.text = "Best Score : " + playerName + " : " + m_Points.ToString();
+            highScore = m_Points;
+            StartMenuManager.Instance.SaveName();
+            StartMenuManager.Instance.SaveScore(highScore);
         }
         m_GameOver = true;
         GameOverText.SetActive(true);
     }
-    [System.Serializable]
+
+    /*[System.Serializable]
     public class SaveData
     {
         public int score;
+        public string playername;
+        public int loadCount;
     }
     public void SaveNameScore(int score)
     {
         SaveData data = new SaveData();
         data.score = score;
+        data.playername = playerName;
+        data.loadCount = ++loadCount; // Save the load count to save previous player name
         string json = JsonUtility.ToJson(data);
 
         File.WriteAllText(Application.persistentDataPath + "/savefile.json", json);
@@ -104,9 +128,10 @@ public class MainManager : MonoBehaviour
         {
             string json = File.ReadAllText(path);
             SaveData data = JsonUtility.FromJson<SaveData>(json);
-
+            data.playername = playerName; // Ensure the player name is loaded correctly
             highScore = data.score;
+            loadCount = data.loadCount; // Load the previous load count
         }
         return highScore;
-    }
+    }*/
 }
--- Assets/Scripts/StartMenuManager.cs
+++ Assets/Scripts/StartMenuManager.cs
@@ -3,12 +3,15 @@ using UnityEngine.UI;
 using TMPro;
 using UnityEditor.Overlays;
 using System.IO;
+using UnityEngine.SceneManagement;
 
 public class StartMenuManager : MonoBehaviour
 {
     public static StartMenuManager Instance;
     public TMP_InputField nameInputField;
     public string playerName;
+    public TMP_Text highScore;
+    public int score;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
 
     public void Awake()
@@ -24,33 +27,76 @@ public class StartMenuManager : MonoBehaviour
 
         LoadName();
     }
+    public void Start()
+    {
+        if (LoadName() != null)
+        {
+            highScore.text = "Best Score : " + LoadName() + " : " + LoadScore().ToString();
+        }
+        else
+        {
+            highScore.text = "Best Score:";
+        }
+    }
+
     [System.Serializable]
     public class SaveData
     {
         public string playername;
+        public int score;
     }
 
     public void SaveName()
     {
-        playerName = nameInputField.text;
+        if (SceneManager.GetActiveScene().buildIndex == 0)
+        {
+            playerName = nameInputField.text;
+            return; // Ensure this only runs in the start menu scene
+        }
+        else
+        { 
+        playerName = LoadName();
+        }
         SaveData data = new SaveData();
         data.playername = playerName;
         string json = JsonUtility.ToJson(data);
 
         File.WriteAllText(Application.persistentDataPath + "/savefile.json", json);
-        //Debug.Log("Name saved: " + playerName);
+        
     }
 
+    public void SaveScore(int score) //TODO: Figure out why score isn't saving
+    {
+        SaveData data = new SaveData();
+        data.score = score;
+        string json = JsonUtility.ToJson(data);
+        File.WriteAllText(Application.persistentDataPath + "/savefile.json", json);
+    }
     public string LoadName()
     {
         string path = Application.persistentDataPath + "/savefile.json";
-        if (File.Exists(path))
+        if (File.Exists(path) && MainManager.Instance != null)
         {
             string json = File.ReadAllText(path);
             SaveData data = JsonUtility.FromJson<SaveData>(json);
 
             playerName = data.playername;
+            
         }
         return playerName;
+    }
+
+    public int LoadScore()
+    {
+        string path = Application.persistentDataPath + "/savefile.json";
+        if (File.Exists(path) && MainManager.Instance != null)
+        {
+            string json = File.ReadAllText(path);
+            SaveData data = JsonUtility.FromJson<SaveData>(json);
+            
+            score = data.score;
+            //highScore.text = "Best Score: " + playerName + " : " + data.score.ToString();
+        }
+        return score;
     }
 }
